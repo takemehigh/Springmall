d136c79a8761b3db1dc50853f25b1ffc705172cd
370503306@qq.com <1994wangge>
2020-07-07 23:21:11 +0800

    21334213

---
+++ src/main/java/com/wg/springmall/SpringmallApplication.java
@@ -0,0 +1,11 @@
+package com.wg.springmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmallApplication.class);
+    }
+}
